0a4c2a3cf90b4d3ad76489fdb9eda784257317de
Ilham Vahrizal <ilham.vahrizal@gmail.com>
2024-01-16 15:31:24 +0700

    code changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Selamat datang di DevOps')
+  res.send('Selamat datang di DevOps Tugas')
 })
 
 app.listen(3000)
